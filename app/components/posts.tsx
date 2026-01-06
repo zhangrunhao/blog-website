a2871522b55eda5144da651eb3e507bf4567d577
zhangrh <runhaozhang@sohu-inc.com>
2026-01-06 09:31:07 +0800

    feat

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -23,7 +23,7 @@ export function BlogPosts() {
           >
             <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
               <p className="text-neutral-600 w-[100px] tabular-nums">
-                {formatDate(post.metadata.publishedAt, false)}
+                {formatDate(post.metadata.publishedAt)}
               </p>
               <p className="text-neutral-900 tracking-tight">
                 {post.metadata.title}
